0f34a307cb784668e4b934b74c47abce440ba773
Subeom Choi <subumm1@gmail.com>
2022-07-21 20:37:11 +0900

    test: develop test for contract

---
+++ Test/Concurrency/Contract/Chain/ContractCatchTest.swift
@@ -0,0 +1,31 @@
+//
+//  ContractCatchTest.swift
+//  SabyConcurrencyTest
+//
+//  Created by WOF on 2022/07/21.
+//
+
+import XCTest
+@testable import SabyConcurrency
+
+final class ContractCatchTest: XCTestCase {
+    func test__catch_return_value() {
+        let contract0 = Contract<Int>()
+
+        let contract = contract0.catch { error in }
+        
+        ContractTest.expect(contract: contract,
+                            state: .resolved(10),
+                            timeout: .seconds(1))
+        {
+            contract0.resolve(10)
+        }
+        
+        ContractTest.expect(contract: contract,
+                            state: .rejected(ContractTest.SampleError.one),
+                            timeout: .seconds(1))
+        {
+            contract0.reject(ContractTest.SampleError.one)
+        }
+    }
+}
