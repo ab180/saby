220897dbe4406a7f98e890910a6b0de0f4cc4927
Young Bin Lee <enebin777@gmail.com>
2022-10-06 11:50:27 +0900

    refactoring: Separate class properties into extension

--- Source/AppleLogger/Logger.swift
+++ Source/AppleLogger/Logger.swift
@@ -16,37 +16,9 @@ public final class Logger {
     var loggerConfiguration: LoggerConfiguration
     var logService: LogService = OSLogService()
     
-    /// A copy of configuration object of this logger
-    public var configuration: LoggerConfiguration {
-        return loggerConfiguration
-    }
-    
-    /// Configures the instance with given configuration
-    public func configure(with configuration: LoggerConfiguration) {
-        loggerConfiguration = configuration
-    }
-    
-    /// Sets the instance's logging level to the given value
-    public func setLogLevel(to level: LogLevel) {
-        loggerConfiguration.logLevel = level
-    }
-    
-    /// Displays a log for specified level
-    func log(level: LogLevel, _ message: String) {
-        guard let loggerLevel = configuration.logLevel else {
-            return
-        }
-        
-        if level.isHigherOrEqual(to: loggerLevel) {
-            if configuration.usePrint {
-                logService.printLog(type: level, message)
-            } else {
-                logService.log("%s", log: level.osLog, type: level.osLogType, message)
-            }
-        }
-    }
-    
     public init(configuration: LoggerConfiguration = LoggerConfiguration.default) {
         self.loggerConfiguration = configuration
     }
 }
+
+
--- Source/AppleLogger/LoggerExtension.swift
+++ Source/AppleLogger/LoggerExtension.swift
@@ -23,3 +23,38 @@ extension Logger {
     }
 }
 
+extension Logger {
+    /// A copy of configuration object of this logger
+    public var configuration: LoggerConfiguration {
+        return loggerConfiguration
+    }
+}
+
+extension Logger {
+    /// Configures the instance with given configuration
+    public func configure(with configuration: LoggerConfiguration) {
+        loggerConfiguration = configuration
+    }
+    
+    /// Sets the instance's logging level to the given value
+    public func setLogLevel(to level: LogLevel) {
+        loggerConfiguration.logLevel = level
+    }
+}
+
+extension Logger {
+    /// Displays a log for specified level
+    func log(level: LogLevel, _ message: String) {
+        guard let loggerLevel = configuration.logLevel else {
+            return
+        }
+        
+        if level.isHigherOrEqual(to: loggerLevel) {
+            if configuration.usePrint {
+                logService.printLog(type: level, message)
+            } else {
+                logService.log("%s", log: level.osLog, type: level.osLogType, message)
+            }
+        }
+    }
+}
