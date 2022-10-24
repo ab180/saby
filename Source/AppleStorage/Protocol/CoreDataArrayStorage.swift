eba7153264d4792842caa624e001f09f7a034810
mjgu <minjae@ab180.co>
2022-10-24 10:15:46 +0900

    working on a async version coredata storage

--- Source/AppleStorage/Protocol/CoreDataArrayStorage.swift
+++ Source/AppleStorage/Protocol/CoreDataArrayStorage.swift
@@ -20,27 +20,55 @@ public final class CoreDataArrayStorage<Item> where
         & ManagedObjectUpdater
     )
 {
-    private let container: NSPersistentContainer
-    private let keyEntityNamed: String
+    private var container: NSPersistentContainer?
+    private var context: NSManagedObjectContext
+    private var keyEntityNamed: String?
     
     public let mockContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
     
-    public init?(bundle: Bundle, modelNamed: String, keyEntityNamed: String) {
-        self.keyEntityNamed = keyEntityNamed
-        
-        guard
-            false == keyEntityNamed.isEmpty,
-            let url = bundle.url(forResource: modelNamed, withExtension: "momd"),
-            FileManager.default.fileExists(atPath: url.path),
-            let managedObjectModel = NSManagedObjectModel(contentsOf: url)
-        else { return nil }
-        
-        self.container = NSPersistentContainer(
-            name: modelNamed, managedObjectModel: managedObjectModel
-        )
-        
-        container.loadPersistentStores { _, error in
-            guard nil == error else { fatalError() }
+    private init() {
+        context = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+    }
+    
+    public class func create(bundle: Bundle, modelNamed: String, keyEntityNamed: String) -> Promise<CoreDataArrayStorage> {
+        return Promise<CoreDataArrayStorage> { resolve, reject in
+            let storage = CoreDataArrayStorage()
+            storage.keyEntityNamed = keyEntityNamed
+            
+            guard
+                false == keyEntityNamed.isEmpty,
+                let url = bundle.url(forResource: modelNamed, withExtension: "momd"),
+                FileManager.default.fileExists(atPath: url.path),
+                let managedObjectModel = NSManagedObjectModel(contentsOf: url)
+            else {
+                reject(URLError(.badURL))
+                return
+            }
+            
+            storage.container = NSPersistentContainer(
+                name: modelNamed, managedObjectModel: managedObjectModel
+            )
+            
+            guard let container = storage.container else {
+                reject(URLError(.fileDoesNotExist))
+                return
+            }
+            
+            container.loadPersistentStores { _, error in
+                if let error = error {
+                    reject(error)
+                    return
+                }
+                
+                if (false) {
+                    storage.context = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+                    storage.context.automaticallyMergesChangesFromParent = true
+                } else if let context = storage.container?.viewContext {
+                    storage.context = context
+                }
+                
+                resolve(storage)
+            }
         }
     }
 }
@@ -49,16 +77,23 @@ extension CoreDataArrayStorage: ArrayStorage {
     public typealias Value = Item
     
     public func push(_ value: Item) {
-        guard let item: Item = Item.creator(context: container.viewContext) else { return }
+        guard let item: Item = Item.creator(context: self.context) else { return }
         item.updateData(value)
     }
     
     public func delete(_ value: Item) {
-        container.viewContext.delete(value)
+        self.context.delete(value)
     }
     
     public func get(key: Item.Key) -> Item? {
-        let predicate = NSPredicate(format: "\(keyEntityNamed) == %@", String(describing: key))
+        let keyString: CVarArg
+        switch key {
+        case let key as UUID: keyString = (key as CVarArg)
+        default: keyString = String(describing: key)
+        }
+        
+        let predicate = NSPredicate(format: "\(keyEntityNamed ?? "") == %@", keyString)
+        print(predicate.predicateFormat)
         return getAll(predicate: predicate).first
     }
     
@@ -71,9 +106,9 @@ extension CoreDataArrayStorage: ArrayStorage {
         }
     }
     
-    public func save() -> SabyConcurrency.Promise<Void> {
-        return Promise<Void>(on: .main) { resolve, reject in
-            try self.container.viewContext.save()
+    public func save() -> Promise<Void> {
+        return Promise<Void> { resolve, reject in
+            try self.context.save()
             resolve(())
         }
     }
@@ -82,7 +117,9 @@ extension CoreDataArrayStorage: ArrayStorage {
 extension CoreDataArrayStorage {
     func removeAll() throws {
         let request = fetchRequest()
-        try container.viewContext.execute(NSBatchDeleteRequest(fetchRequest: request))
+        try self.context.execute(NSBatchDeleteRequest(fetchRequest: request))
+        
+        
     }
 }
 
@@ -96,7 +133,7 @@ extension CoreDataArrayStorage {
     private func getAll(predicate: NSPredicate? = nil, limit: Int? = nil) -> [Item] {
         let request = fetchRequest(limit: limit)
         request.predicate = predicate
-        let items = try? container.viewContext.fetch(request) as? [Item]
+        let items = try? self.context.fetch(request) as? [Item]
         return items ?? []
     }
 }
