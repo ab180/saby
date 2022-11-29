40e2bbe5638e594af194b5dab8016fd397f75e97
Subeom Choi <subumm1@gmail.com>
2022-11-29 16:12:12 +0900

    feature(concurrency/contract): add filter promise optional value

--- Source/Concurrency/Contract/Chain/ContractFilter.swift
+++ Source/Concurrency/Contract/Chain/ContractFilter.swift
@@ -44,4 +44,52 @@ extension Contract {
         
         return contract
     }
+    
+    public func filter<Result>(on queue: DispatchQueue? = nil,
+                               _ block: @escaping (Value) -> Promise<Result?>) -> Contract<Result>
+    {
+        let queue = queue ?? self.queue
+        
+        let contract = Contract<Result>(on: queue)
+        
+        subscribe(subscriber: Subscriber(
+            on: queue,
+            onResolved: { value in
+                block(value).subscribe(subscriber: Promise.Subscriber(
+                    on: queue,
+                    onResolved: { result in
+                        guard let result else { return }
+                        contract.resolve(result)
+                    },
+                    onRejected: { contract.reject($0) }
+                ))
+            },
+            onRejected: { error in contract.reject(error) }
+        ))
+        
+        return contract
+    }
+    
+    public func filter<Result>(on queue: DispatchQueue? = nil,
+                               _ block: @escaping (Value) -> Promise<Result>?) -> Contract<Result>
+    {
+        let queue = queue ?? self.queue
+        
+        let contract = Contract<Result>(on: queue)
+        
+        subscribe(subscriber: Subscriber(
+            on: queue,
+            onResolved: { value in
+                guard let valuePromise = block(value) else { return }
+                valuePromise.subscribe(subscriber: Promise.Subscriber(
+                    on: queue,
+                    onResolved: { contract.resolve($0) },
+                    onRejected: { contract.reject($0) }
+                ))
+            },
+            onRejected: { error in contract.reject(error) }
+        ))
+        
+        return contract
+    }
 }
