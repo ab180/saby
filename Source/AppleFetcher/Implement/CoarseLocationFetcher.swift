fd9d6a07a646ba3e8d3c0f99f032b0c20870d273
Subeom Choi <subumm1@gmail.com>
2024-01-03 15:00:04 +0900

    refactor(SabyAppleFetcher): fetch locale without UserDefaults

--- Source/AppleFetcher/Implement/CoarseLocationFetcher.swift
+++ Source/AppleFetcher/Implement/CoarseLocationFetcher.swift
@@ -14,7 +14,9 @@ import SabyConcurrency
 public final class CoarseLocationFetcher: Fetcher {
     public typealias Value = CoarseLocation
     
-    public init() {}
+    public init() {
+        savePreferredLanguages()
+    }
 
     public func fetch() -> CoarseLocation {
         CoarseLocation(
@@ -31,13 +33,71 @@ public struct CoarseLocation {
 
 extension CoarseLocationFetcher {
     private func fetchLocale() -> String {
-        (UserDefaults(suiteName: "SabyAppleFetcher")?
-            .stringArray(forKey: "AppleLanguages")?
-            .first ?? Locale.autoupdatingCurrent.identifier)
+        guard
+            let language = fetchLanguage(),
+            let region = fetchRegion()
+        else {
+            return Locale.autoupdatingCurrent.identifier
+        }
+        
+        return "\(language)-\(region)"
     }
     
     private func fetchTimezone() -> String {
         TimeZone.autoupdatingCurrent.identifier
+    }
+}
+
+extension CoarseLocationFetcher {
+    private func fetchLanguage() -> String? {
+        let preferredLanguage = readPreferredLanguages()
+        
+        guard let source = preferredLanguage.first else {
+            return nil
+        }
+        
+        guard let barIndex = source.firstIndex(of: "-") else {
+            return source
+        }
+        
+        return String(source[..<barIndex])
+    }
+    
+    private func fetchRegion() -> String? {
+        Locale.autoupdatingCurrent.regionCode
+    }
+}
+
+extension CoarseLocationFetcher {
+    private var url: URL? {
+        FileManager.default.urls(
+            for: .libraryDirectory,
+            in: .userDomainMask
+        )
+        .first?
+        .appendingPathComponent("saby")
+        .appendingPathComponent("SabyAppleFetcher_Locale_PreferredLanguages")
+    }
+    
+    private func savePreferredLanguages() {
+        guard let url else { return }
+        let directoryURL = url.deletingLastPathComponent()
+        
+        let fileManager = FileManager.default
+        if !fileManager.fileExists(atPath: directoryURL.absoluteString) {
+            try? fileManager.createDirectory(at: directoryURL, withIntermediateDirectories: true)
+        }
+        
+        try? JSONEncoder().encode(Locale.preferredLanguages).write(to: url)
+    }
+    
+    private func readPreferredLanguages() -> [String] {
+        guard let url else { return Locale.preferredLanguages }
+        
+        return (
+            (try? JSONDecoder().decode([String].self, from: Data(contentsOf: url)))
+            ?? Locale.preferredLanguages
+        )
     }
 }
 
